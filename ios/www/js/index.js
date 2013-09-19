206bf729d8672ed64dde42fc5a54876b2eefd1ae
Steven Roseman <stevierose@fullsail.edu>
2013-09-18 21:54:39 -0400

    updated js and html files

--- ios/www/js/index.js
+++ ios/www/js/index.js
@@ -1,6 +1,6 @@
 
 $(function() {
-  var tag = "bars",
+  var tag = "kittens",
   
   url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=134427b420784a59ad7cddba9e547892";
   
@@ -11,16 +11,18 @@ $(function() {
 
 var screenOutput = function(bStats) {
 	
-	console.log(bStats);
     
-    
-	$.each(bStats.data , function(index, photo) {
-           var pic = "'<li><img src='" + photo.images.standard_resolution.url + "' alt= '" + photo.user.id
-           + "' /><h4>" +photo.user.full_name +", <em>(" + photo.user.username +")</em></h4></li>
-           ";
-           $("#data-output").append(pic);
+	$.each(bStats.data, function(index, photo) {
            
-           }); 
+           
+           var pic = "<li><img src ='" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "'/><li>";
+           console.log(pic);
+           $("#data-msg").append(pic);
+           });
+    
+    
+    
+    
     
     
 };
