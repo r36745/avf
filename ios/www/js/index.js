fe7461f8b3df1ce9c0af5b154761a1e5fb911d12
Steven Roseman <stevierose@fullsail.edu>
2013-09-22 23:17:39 -0400

    updated ios files

--- ios/www/js/index.js
+++ ios/www/js/index.js
@@ -1,39 +1,49 @@
 
+
 $(function() {
-var tag = "bars",
+  
+  
+  var bUrl = "http://api.brewerydb.com/v2/featured/?key=851b96f1795e0c133ec62aca2bc49ab4";
+  
+  $.getJSON(bUrl, beerOutput);
+  });
 
-	url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=134427b420784a59ad7cddba9e547892";
+var beerOutput = function(info) {
+    console.log(info);
+    $.each(info.data.brewery.images, function(index, photo){
+           console.log(info.data);
+           var beer = "<li><img src='" + photo + "'/></li>";
+           console.log(beer);
+           $("#beer-data").append(beer);
+           });
+    
+};
 
-$.getJSON(url, screenOutput); 
-});	
-	
+
+$(function() {
+  var tag = "restaurant",
+  
+  url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=134427b420784a59ad7cddba9e547892";
+  
+  $.getJSON(url, screenOutput);
+  });
+
 
 
 var screenOutput = function(bStats) {
 	
-
+    
 	$.each(bStats.data, function(index, photo) {
-		
-
-		var pic = "<li><img src ='" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "'/><li>";
-		console.log(pic);
-		$("#data-output").append(pic);
-	});
-
-
-
-	 
-		
-
+           
+           
+           var pic = "<li><img src ='" + photo.images.standard_resolution.url + "' alt='" + photo.user.id + "'/><li>";
+           console.log(pic);
+           $("#data-msg").append(pic);
+           });
+    
+    
+    
+    
+    
+    
 };
-
-$(function(){
-	var eUrl = "http://api.espn.com/:version/sports/:sportname?apikey=:uh3yz7wpdhatdpyqj2ykg8j3";
-
-	$.getJSON(eUrl, espnOutput);
-});
-
-var espnOutput = function(info) {
-	console.log(info);
-};
-
